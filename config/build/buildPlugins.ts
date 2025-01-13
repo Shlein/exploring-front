4c6ecc7298eadaf77a5f69239fd76da76e3b8ef0
shlein <shd@newton.finance>
2025-01-13 20:10:20 +0800

    16 lesson (hot module replacement)

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,8 +3,10 @@ import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BuildOptions } from './types/config'
 
-export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-
+export function buildPlugins({
+  paths,
+  isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HtmlWebpackPlugin({
       template: paths.html,
@@ -12,10 +14,11 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css'
+      chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ]
 }
