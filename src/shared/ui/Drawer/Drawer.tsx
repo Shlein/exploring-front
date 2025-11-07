47b15adf4572c5b288be3fcf270c1c61342bacdb
shlein <shd@newton.finance>
2025-11-07 11:32:56 +0800

    81 - useModal for Drawer and Modal

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,8 +1,9 @@
-import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import { memo, ReactNode } from 'react';
+import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { Portal } from '../Portal';
+import { useModal } from 'shared/lib/hooks/useModal/useModal';
 import { Overlay } from '../Overlay/Overlay';
+import { Portal } from '../Portal';
 import cls from './Drawer.module.scss';
 
 interface DrawerProps {
@@ -10,15 +11,27 @@ interface DrawerProps {
   children: ReactNode;
   isOpen?: boolean;
   onClose?: () => void;
+  lazy?: boolean;
 }
 
 export const Drawer = memo((props: DrawerProps) => {
-  const { className, children, onClose, isOpen } = props;
+  const { className, children, onClose, isOpen, lazy } = props;
+
+  const { close, isClosing, isMounted } = useModal({
+    animationDelay: 300,
+    onClose: onClose,
+    isOpen: isOpen
+  });
   const { theme } = useTheme();
 
   const mods: Mods = {
-    [cls.opened]: isOpen
+    [cls.opened]: isOpen,
+    [cls.isClosing]: isClosing
   };
+
+  if (lazy && !isMounted) {
+    return null;
+  }
 
   return (
     <Portal>
@@ -29,7 +42,7 @@ export const Drawer = memo((props: DrawerProps) => {
           'app_drawer'
         ])}
       >
-        <Overlay onClick={onClose} />
+        <Overlay onClick={close} />
         <div className={cls.content}>{children}</div>
       </div>
     </Portal>
--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -1,16 +1,10 @@
+import { ReactNode } from 'react';
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
-import React, {
-  MutableRefObject,
-  ReactNode,
-  useCallback,
-  useEffect,
-  useRef,
-  useState
-} from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
-import cls from './Modal.module.scss';
 import { Portal } from 'shared/ui/Portal';
 import { Overlay } from 'shared/ui/Overlay/Overlay';
+import { useModal } from 'shared/lib/hooks/useModal/useModal';
+import cls from './Modal.module.scss';
 
 interface ModalProps {
   className?: string;
@@ -24,50 +18,13 @@ const ANIMATION_DELAY = 300;
 
 export const Modal = (props: ModalProps) => {
   const { className, children, isOpen, onClose, lazy } = props;
+  const { close, isClosing, isMounted } = useModal({
+    animationDelay: ANIMATION_DELAY,
+    onClose: onClose,
+    isOpen: isOpen
+  });
 
-  const [isClosing, setIsClosing] = useState(false);
-  const [isMounted, setIsMounted] = useState(false);
-  const timerRef = useRef() as MutableRefObject<
-    ReturnType<typeof setTimeout>
-  >;
   const { theme } = useTheme();
-
-  useEffect(() => {
-    if (isOpen) {
-      setIsMounted(true);
-    }
-  }, [isOpen]);
-
-  const closeHandler = useCallback(() => {
-    if (onClose) {
-      setIsClosing(true);
-      timerRef.current = setTimeout(() => {
-        onClose();
-        setIsClosing(false);
-      }, ANIMATION_DELAY);
-    }
-  }, [onClose]);
-
-  // Новые ссылки!!!
-  const onKeyDown = useCallback(
-    (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        closeHandler();
-      }
-    },
-    [closeHandler]
-  );
-
-  useEffect(() => {
-    if (isOpen) {
-      window.addEventListener('keydown', onKeyDown);
-    }
-
-    return () => {
-      clearTimeout(timerRef.current);
-      window.removeEventListener('keydown', onKeyDown);
-    };
-  }, [isOpen, onKeyDown]);
 
   const mods: Mods = {
     [cls.opened]: isOpen,
@@ -87,7 +44,7 @@ export const Modal = (props: ModalProps) => {
           'app_modal'
         ])}
       >
-        <Overlay onClick={closeHandler} />
+        <Overlay onClick={close} />
         <div className={cls.content}>{children}</div>
       </div>
     </Portal>
